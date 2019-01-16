0fefc211c2e1445c47a6b3b3bcb6b110ce732a61
Abhinav Kumar <kumarabhinav75@gmail.com>
2019-01-16 19:39:26 +0530

    Solved warmup from promise exercise

--- promise-shop/warmup.js
+++ promise-shop/warmup.js
@@ -1,13 +1,1 @@
-Parse.User.logIn('user', 'pass', {
-    success: function (user) {
-      query.find({
-        success: function (results) {
-          results[0].save({ key: value }, {
-            success: function (result) {
-              // the object was saved
-            }
-          });
-        }
-      });
-    }
-  });
+setTimeout(() => console.log('TIMED OUT!'),300);
