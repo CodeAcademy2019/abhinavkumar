8fa575faecb6ce249b8bb278e7e1062f47464724
Abhinav Kumar <kumarabhinav75@gmail.com>
2019-01-18 09:41:59 +0530

    Solved 4th problwm with test case

--- promise-shop/p4.test.js
+++ promise-shop/p4.test.js
@@ -1,16 +1,15 @@
-const reject = require('./rejectOrNot');
+const rejected = require('./rejectOrNot');
 
-const prm = reject();
 
 
 test('Test to check if I FIRED printed!', () => {
-    expect.assertions(1);
+    //expect.assertions(1);
 
-    return expect(prm).resolves.toEqual('I FIRED');
+    return expect(rejected).resolves.toEqual('I FIRED');
 });
 
 test('Test to check I DID NOT FIRE not printed', () => {
-    expect.assertions(1);
+    //expect.assertions(1);
 
-    return expect(prm).resolves.not.toEqual('I DID NOT FIRE');
+    return expect(rejected).resolves.not.toEqual('I DID NOT FIRE');
 });
--- promise-shop/rejectOrNot.js
+++ promise-shop/rejectOrNot.js
@@ -1,13 +1,15 @@
-let rejectOrNot = () =>  { 
-    let promise = new Promise(function(fulfill,reject){
+function rejectOrNot(){ 
+
+    let constructPromise = (fulfill,reject) => {
         fulfill('I FIRED');
         reject(new Error('I DID NOT FIRE'));
-    })
+    };
 
     let onRejected = (error) => console.log(error.message);
 
+    let promise = new Promise(constructPromise);
     promise.then(console.log,onRejected);
     return promise;
 }
-rejectOrNot();
-module.exports = rejectOrNot;
+
+module.exports = rejectOrNot();
