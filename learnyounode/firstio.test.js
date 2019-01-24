b661907bfd8bdceec3fe0615feb0219cb2996281
Abhinav Kumar <kumarabhinav75@gmail.com>
2019-01-24 15:30:08 +0530

    changed test descriptions

--- learnyounode/firstio.test.js
+++ learnyounode/firstio.test.js
@@ -1,13 +1,13 @@
 const numOfNewLines = require('./firstio');
 
-test('Testing the function to correctly calculate the number of new lines in the testfile', () => {
-    expect(numOfNewLines('./test.txt')).toEqual(4)
+test('Testing the function to correctly calculate the number of new lines with 4 newlines with text', () => {
+  expect(numOfNewLines('./test.txt')).toEqual(4);
 });
 
-test('Testing the function to correctly calculate the number of new lines in the testfile2 ', () => {
-    expect(numOfNewLines('./test1.txt')).toEqual(3)
+test('Testing the function to correctly calculate the number of newlines with 2 newlines before text ', () => {
+  expect(numOfNewLines('./test1.txt')).toEqual(3);
 });
 
-test('Testing the function to correctly calculate the number of new lines in the testfile2 ', () => {
-    expect(numOfNewLines('./test2.txt')).toEqual(5)
+test('Testing the function to correctly calculate the number of new lines with only newlines ', () => {
+  expect(numOfNewLines('./test2.txt')).toEqual(5);
 });
