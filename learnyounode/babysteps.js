16527f13803c4c3721a97e36d3d81ab45369ffb1
Abhinav Kumar <kumarabhinav75@gmail.com>
2019-01-24 15:22:42 +0530

    Added more tests for 2nd question

--- learnyounode/babysteps.js
+++ learnyounode/babysteps.js
@@ -1,6 +1,9 @@
 // const inputs = process.argv.slice(2);
 
 function findSum(inputs) {
+  if(inputs.length === 0){
+    return 0;
+  }
   let sum = 0;
   inputs.forEach(input => sum += Number(input) );
   // console.log(sum);
--- learnyounode/babysteps.test.js
+++ learnyounode/babysteps.test.js
@@ -1,9 +1,25 @@
 const findSum = require('./babysteps');
 
 test('Test to check if sum calculated correctly', () => {
-    expect(findSum(['1','2','3','4','5'])).toEqual(15);
-})
+  expect(findSum([1, 2, 3, 4, 5])).toEqual(15);
+});
 
 test('Test to check if sum calculated correctly with inputs all zeros', () => {
-    expect(findSum(['0','0','0','0'])).toEqual(0);
-})
+  expect(findSum([0, 0, 0, 0, 0, 0, 0, 0, 0, 0])).toEqual(0);
+});
+
+test('Test to check if sum calculated correctly with inputs containing negative numbers', () => {
+  expect(findSum([-1, 0, 1, 0, -3])).toEqual(-3);
+});
+
+test('Test to check if sum calculated correctly with 1 array element', () => {
+  expect(findSum([-1])).toEqual(-1);
+});
+
+test('Test to check if sum calculated correctly with 1 array element', () => {
+  expect(findSum([-1])).toEqual(-1);
+});
+
+test('Test to check if sum calculated correctly with no array element', () => {
+  expect(findSum([])).toEqual(0);
+});
